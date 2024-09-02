3fec46e4a0d1733f3aacaa8475418e466a4f8f1b
Emanoel201106 <emanoelcosmemiguel123@gmail.com>
2024-09-02 14:32:50 -0300

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,15 +16,15 @@
         <footer class="footer py-4">
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2024</div>
+                    <div class="col-lg-4 text-lg-start">Copyright &copy; CRUD 2024</div>
                     <div class="col-lg-4 my-3 my-lg-0">
                     <a id="logo" class="btn btn-dark btn-social mx-2" href="https://web.whatsapp.com/" aria-label="Whatsapp"><img class="logo" src="{{url('assets/img/logo1.png')}}"/></a>
                         <a id="logo" class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/" aria-label="Instagram"><img class="logo" src="{{url('assets/img/logo2.png')}}"/></a>
                         <a id="logo" class="btn btn-dark btn-social mx-2" href="https://github.com/" aria-label="GitHub"><img class="logo" src="{{url('assets/img/logo3.png')}}"/></a>
                     </div>
                     <div class="col-lg-4 text-lg-end">
-                        <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                        <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+                        <a class="link-dark text-decoration-none me-3" href="">Política de Privacidade</a>
+                        <a class="link-dark text-decoration-none" href="">Termos de Uso</a>
                     </div>
                 </div>
             </div>
